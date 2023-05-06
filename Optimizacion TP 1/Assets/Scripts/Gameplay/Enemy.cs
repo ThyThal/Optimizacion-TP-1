2a712930c54fe568a60db1140f1b65088b12f987
Raidonlxl <gonsanchez168@gmail.com>
2023-05-06 19:50:05 -0300

    ApplyNonAllocAPI
    nonAllocAPI: colision enemigo player

--- Optimizacion TP 1/Assets/Scripts/Gameplay/Enemy.cs
+++ Optimizacion TP 1/Assets/Scripts/Gameplay/Enemy.cs
@@ -9,15 +9,33 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject bulletPrefab;
     Vector3 _dir;
     Rigidbody _rb;
-
-    void Start()
+    [SerializeField] private Vector3 sizeHalfEnemy;
+    [SerializeField] private LayerMask maskCollitionAround;
+    [SerializeField] private Collider[] otherCollider = new Collider[3];
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        _dir = Vector3.forward;
+        Move();
+
+        int hitCount = Physics.OverlapBoxNonAlloc(transform.position, sizeHalfEnemy, otherCollider,Quaternion.identity, maskCollitionAround);
+        for (int i = 0; i < hitCount; i++)
+        {
+            if(otherCollider[i].gameObject.layer == 6)
+            {
+                //ME DESTRUYO CON EL PLAYER
+                Destroy(this.gameObject);
+                Debug.Log("Hit CHOCASTE CON EL PLAYER " + otherCollider[i].gameObject.name);
+            }
+
+            ChangeDirection(otherCollider[i].gameObject.layer);
+        }
+
+       
     }
 
     public void Move()
@@ -25,9 +43,30 @@ public class Enemy : MonoBehaviour
         _rb.velocity = _dir.normalized * speed;
     }
 
-    public void ChangeDirection()
+    public void ChangeDirection(LayerMask layerWall)
     {
         // Cambiar direccion al chocar con una pared
+        if (layerWall == 8)
+        {
+            Debug.Log("ENTRO");
+            _dir = -Vector3.forward;
+            Rotate(_dir);
+        }
+        else if (layerWall == 8)
+        {
+            _dir = -Vector3.right;
+            Rotate(_dir);
+        }
+        else if (layerWall==8)
+        {
+            _dir = -Vector3.forward;
+            Rotate(_dir);
+        }
+        else if (layerWall==8)
+        {
+            _dir = Vector3.right;
+            Rotate(_dir);
+        }
     }
 
     public void Rotate(Vector3 lookDir)
